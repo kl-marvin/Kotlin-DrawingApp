b23a325c33e79f98546d98e15dcbbe7428058c66
Marvin K. Langlois <klm@macbook-pro-de-marvin.home>
2020-05-13 08:28:26 +0200

    implementating brush size dialog

--- app/src/main/java/fr/kotlin/letsdraw/MainActivity.kt
+++ app/src/main/java/fr/kotlin/letsdraw/MainActivity.kt
@@ -1,8 +1,10 @@
 package fr.kotlin.letsdraw
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +12,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         drawing_view.setSizeforBrunsh(20.toFloat())
+
+        iBtn_brush.setOnClickListener {
+            showBrushSizeDialog()
+        }
+
 
 
     }
+
+    private fun showBrushSizeDialog(){
+
+        val dialog = Dialog(this) // context in main activy
+        dialog.setContentView(R.layout.dialog_brush_size)
+        dialog.setTitle("Taille du pinceau :")
+
+        val smallBtn = dialog.iBtn_small_brush
+        val mediumBtn = dialog.iBtn_medium_brush
+        val largeBtn = dialog.iBtn_large_brush
+
+        smallBtn.setOnClickListener{
+            drawing_view.setSizeforBrunsh(5.toFloat())
+            dialog.dismiss()
+        }
+        mediumBtn.setOnClickListener{
+            drawing_view.setSizeforBrunsh(15.toFloat())
+            dialog.dismiss()
+        }
+        largeBtn.setOnClickListener{
+            drawing_view.setSizeforBrunsh(25.toFloat())
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 }
